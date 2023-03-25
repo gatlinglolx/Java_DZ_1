15cf07257b01d940676827676960e2f1bb4471e1
vladislav shakhov <shahov199@gmail.com>
2023-03-25 14:09:36 +0300

    zadacha1_2.0

--- zadacha1.java
+++ zadacha1.java
@@ -1,16 +1,18 @@
+import java.util.Arrays;
 public class zadacha1 {
 // Задать целочисленный массив, состоящий из элементов 0 и 1.
 // Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. 
 // С помощью цикла и условия заменить 0 на 1, 1 на 0;
     public static void main(String[] args) {
         int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
-        for (int i : arr) {
+        for (int i = 0;  i < arr.length; i++) {
             if (arr[i] == 1) {
                 arr[i] = 0;
             } else {
                 arr[i] = 1;
             }
         }
+        System.out.println(Arrays.toString(arr));
     }
     
 }
